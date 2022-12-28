c31086ef837c2c16a8c8874115ce33140e4a3f3e
Trainee (Mohammed Abdulla Mohammed Hassan Alhosani) <MAB52339@emiratesnbd.com>
2022-12-28 10:30:07 +0400

    Added check for dividing by 0 and altered check for empty parameter

--- api_server/modules/invalidRequest.js
+++ api_server/modules/invalidRequest.js
@@ -1,13 +1,13 @@
 const invalidRequest = (req, res) => {
 
     // To catch if an entered value is empty
-    if(req.body.number1.length === 0){
+    if(String(req.body.number1).replace(/\s+/g, "").length == 0){
         return 'No value entered for number 1';
     }
-    if(req.body.number2.length === 0){
+    if(String(req.body.number2).replace(/\s+/g, "").length == 0){
         return 'No value entered for number 2';
     }
-    if(req.body.operator.length === 0){
+    if(String(req.body.operator).replace(/\s+/g, "").length == 0){
         return 'No value entered for operator';
     }
 
@@ -22,6 +22,11 @@ const invalidRequest = (req, res) => {
     // If length is 1 and correct operator is passed, value is valid
     if(req.body.operator.length != 1 || !/\+|\-|\/|\*/g.test(req.body.operator)){
         return "Invalid operator passed";
+    }
+
+    // So that dividing by 0 is not allowed
+    if(req.body.operator === '/' && req.body.number2*1 == 0){
+        return "Attempting to divide by 0"
     }
 
     return null;
